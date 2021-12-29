2db492dc904a0215f1dcc32401e9067044718aa3
Oskar0019 <oskarp0619@gmail.com>
2021-12-29 15:22:54 +0100

    Nästan klar med Djikstra (Ej testat)

--- src/projekt/City.java
+++ src/projekt/City.java
@@ -1,9 +1,10 @@
 package projekt;
 
 
-public class City {
+public class City implements Comparable<City>{
 	//attributes
-	String name;
+	private String name;
+	private int weight;
 	
 	public City(String name) {
 		this.name = name;
@@ -11,5 +12,25 @@ public class City {
 	
 	public String toString() {
 		return name;
+	}
+	
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	
+	public int getWeight() {
+		return weight;
+	}
+
+	@Override
+	public int compareTo(City otherCity) {
+		if (this.getWeight() < otherCity.getWeight()) {
+			return -1;
+		}
+		if (this.getWeight() > otherCity.getWeight()) {
+			return 1;
+		}
+		
+		return 0;
 	}
 }
--- src/projekt/Djikstra.java
+++ src/projekt/Djikstra.java
@@ -4,40 +4,42 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 public class Djikstra {
 	
 	private static Map<City, Integer> distanceMap = new HashMap<City, Integer>();
 	private static Map<City, City> predMap = new HashMap<City, City>();
-	private static BSTreePriorityQueue<Node> pq = new BSTreePriorityQueue<Node>();
+	private static PriorityQueue<City> pq = new PriorityQueue<City>();
 	
 	
-	public static CityMap calculateShortestPath(CityMap map, City startCity) {
+	public static Map calculateShortestPath(CityMap map, City startCity) {
 		for (City city : map.getAllCitys()) {
-			predMap.putIfAbsent(city, value);
+			predMap.putIfAbsent(city, null);
 
 			if (city != startCity) {
 				distanceMap.putIfAbsent(city, Integer.MAX_VALUE);
 			} else {
 				distanceMap.putIfAbsent(city, 0);
 			}
-			Node node = new Node(city, distanceMap.get(city));
-			pq.enqueue(node);
+			city.setWeight(distanceMap.get(city));
+			pq.add(city);
 		}
 		while (!pq.isEmpty()) {
-			Node u = pq.dequeue();
-			for (Branch branch : map.getAllNeighbours(u.getCity())) {
-				City currentCity = u.getCity();
+			City currentCity = pq.poll();
+			for (Branch branch : map.getAllNeighbours(currentCity)) {
 				City neighbourCity = branch.getEndCity();
 				if (distanceMap.get(neighbourCity).compareTo(distanceMap.get(currentCity) + branch.getDistance()) > 0) {
 					distanceMap.replace(neighbourCity, (distanceMap.get(currentCity) + branch.getDistance()));
 					predMap.replace(neighbourCity, currentCity);
-					
+					pq.remove(neighbourCity);
+					neighbourCity.setWeight(distanceMap.get(currentCity) + branch.getDistance());
+					pq.add(neighbourCity);
 				}
 			}
 			
 		}		
-		return null;
+		return distanceMap;
 	}
 	
 
